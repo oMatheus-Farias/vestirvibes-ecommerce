c54484d10d262fa6ffb134bbc72725ffeea3a854
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-11 19:39:50 -0300

    feat: add Navigate Menu Component

--- src/components/mobile-menu/mobile-menu.component.tsx
+++ src/components/mobile-menu/mobile-menu.component.tsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { IoMenuOutline } from "react-icons/io5";
 
 //Component
 import Cart from "../cart/cart.component";
+import NavigateMenu from "../navigate-menu/navigate-menu.component";
 
 const MobileMenu = () => {
+  const [visibleMenu, setVisibleMenu] = useState(false);
+
   return (
-    <div className="p-4 bg-primary w-full h-[3.75em] flex justify-between">
-      <IoMenuOutline className="cursor-pointer" size={30} color="#000" />
+    <>
+      <NavigateMenu
+        visible={visibleMenu}
+        closeMenu={() => setVisibleMenu(false)}
+      />
 
-      <img src="logo.svg" alt="logo VestirVibes" className="cursor-pointer" />
+      <div className="p-4 bg-primary w-full h-[3.75em] flex justify-between">
+        <button className="cursor-pointer" onClick={() => setVisibleMenu(true)}>
+          <IoMenuOutline size={30} color="#000" />
+        </button>
 
-      <Cart />
-    </div>
+        <img src="logo.svg" alt="logo VestirVibes" className="cursor-pointer" />
+
+        <Cart />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/navigate-menu/navigate-menu.component.tsx
@@ -0,0 +1,37 @@
+import { IoCloseOutline } from "react-icons/io5";
+
+interface NavigateMenuProps {
+  visible: boolean;
+  closeMenu: () => void;
+}
+
+const NavigateMenu = ({ visible, closeMenu }: NavigateMenuProps) => {
+  return (
+    <section
+      className="flex justify-between absolute w-full h-screen z-10 transition-all duration-300"
+      style={{
+        visibility: visible ? "visible" : "hidden",
+        opacity: visible ? 1 : 0,
+      }}
+    >
+      <div className="w-[86%] bg-white">
+        <div className="p-4 flex justify-between items-center bg-primary h-[3.75em]">
+          <img src="logo.svg" alt="logo VestirVibes" />
+
+          <button className="cursor-pointer" onClick={closeMenu}>
+            <IoCloseOutline size={30} color="#000" />
+          </button>
+        </div>
+
+        <nav className="mt-10 px-5 flex flex-col gap-4 w-full">
+          <button className="text-start font-bold">Explorar</button>
+          <button className="text-start font-bold">Login</button>
+          <button className="text-start font-bold">Criar Conta</button>
+        </nav>
+      </div>
+      <div className="w-[14%] bg-transparentBlack" onClick={closeMenu}></div>
+    </section>
+  );
+};
+
+export default NavigateMenu;
