38db2b66efe75555bb7d88bfb1d9f275300454f1
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-11 14:56:54 -0300

    style: change global styles and add Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
