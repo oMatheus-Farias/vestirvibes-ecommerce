12b79d47ff4233f78241b8c798def11a756041b5
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-11 15:16:48 -0300

    feat: initial config React Houter Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/home/home.page";
+
 function App() {
   return (
-    <>
-      <h1>Page App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
