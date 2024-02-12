69ad50c178c050d46f64b1ba49775324738e3abd
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-11 21:52:51 -0300

    feat: add Footer Component

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="w-full h-28 bg-dark flex items-center justify-center px-4">
+      <p className="text-xs text-white text-center">
+        Copyright © 2024 | Vestir Vibes - Todos os direitos reservados
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
