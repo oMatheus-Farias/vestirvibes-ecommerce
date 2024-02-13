6fdb7c8f1e3a8d89fc17e3d5b9e762eb96dac24d
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-12 22:23:29 -0300

    feat: add Customer Button Component

---
+++ src/components/customer-button/customer-button.component.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, ButtonHTMLAttributes } from "react";
+
+interface CustomerButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color: string;
+  icon: ReactNode;
+  children: string;
+}
+
+const CustomerButton = ({ color, icon, children }: CustomerButtonProps) => {
+  return (
+    <button
+      className="w-full h-11 rounded-[10px] text-white font-semibold flex items-center justify-center gap-2"
+      style={{ backgroundColor: color }}
+    >
+      {icon}
+      {children}
+    </button>
+  );
+};
+
+export default CustomerButton;
