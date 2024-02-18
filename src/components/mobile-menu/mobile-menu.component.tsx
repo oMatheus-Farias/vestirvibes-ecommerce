22327c152dc7bc2cc73fe2425295d2de96b4e0c2
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-18 14:06:52 -0300

    feat: ensure that the mobile menu is only opened when the cart menu is closed

--- src/components/mobile-menu/mobile-menu.component.tsx
+++ src/components/mobile-menu/mobile-menu.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 
 //Icon
@@ -8,11 +8,16 @@ import { IoMenuOutline } from "react-icons/io5";
 import Cart from "../cart/cart.component";
 import NavigateMenu from "../navigate-menu/navigate-menu.component";
 
+//Utilitie
+import { CartContext } from "../../contexts/cart.context";
+
 interface MobileMenuProps {
   logo?: string;
 }
 
 const MobileMenu = ({ logo }: MobileMenuProps) => {
+  const { isVisible } = useContext(CartContext);
+
   const [visibleMenu, setVisibleMenu] = useState(false);
 
   return (
@@ -24,7 +29,10 @@ const MobileMenu = ({ logo }: MobileMenuProps) => {
       />
 
       <header className="p-4 bg-primary w-full h-[3.75em] flex justify-between">
-        <button className="cursor-pointer" onClick={() => setVisibleMenu(true)}>
+        <button
+          className="cursor-pointer"
+          onClick={() => setVisibleMenu(!isVisible && true)}
+        >
           <IoMenuOutline size={30} color="#000" />
         </button>
 
