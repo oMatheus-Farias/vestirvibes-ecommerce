a8f8fb62464705e311011714cc08557156c2d326
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-18 13:55:04 -0300

    style: improve the carousel experience on mobile devices

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -37,47 +37,49 @@ const HomePage = () => {
         />
       </div>
 
-      <main className="px-4 flex-1 flex flex-col items-center sm:px-6">
-        <div className="mt-10 flex items-center gap-4 w-full overflow-auto max-w-[62.5em] [&::-webkit-scrollbar]:hidden">
+      <main className="flex-1 flex flex-col items-center ">
+        <div className="mt-10 flex items-center gap-4 w-full overflow-auto max-w-[62.5em] [&::-webkit-scrollbar]:hidden px-4">
           {categories.map((category) => {
             return <Categories key={category.id} category={category} />;
           })}
         </div>
 
-        <h1 className="text-center text-black font-bold text-2xl mt-10">
-          Quem Somos?
-        </h1>
+        <div className="px-4 sm:px-6">
+          <h1 className="text-center text-black font-bold text-2xl mt-10">
+            Quem Somos?
+          </h1>
 
-        <section className="mt-10 flex flex-col gap-10 w-full justify-between max-w-[62.5em] md:flex-row">
-          <div>
-            <h2 className="text-xl font-medium">VestVibes</h2>
+          <section className="mt-10 flex flex-col gap-10 w-full justify-between max-w-[62.5em] md:flex-row">
+            <div>
+              <h2 className="text-xl font-medium">VestVibes</h2>
 
-            <p className="mt-2 max-w-[31.25em]">
-              Explore o universo da moda conosco na VestVibes - sua loja online
-              para estilo e elegância. Descubra coleções modernas e exclusivas,
-              escolha entre roupas casuais e peças para ocasiões especiais.
-              Garantimos uma experiência de compra segura e fácil, entregando
-              moda de alta qualidade diretamente à sua porta. Reinvente seu
-              estilo conosco - seja bem-vindo ao seu novo destino de moda
-              online!
-            </p>
+              <p className="mt-2 max-w-[31.25em]">
+                Explore o universo da moda conosco na VestVibes - sua loja
+                online para estilo e elegância. Descubra coleções modernas e
+                exclusivas, escolha entre roupas casuais e peças para ocasiões
+                especiais. Garantimos uma experiência de compra segura e fácil,
+                entregando moda de alta qualidade diretamente à sua porta.
+                Reinvente seu estilo conosco - seja bem-vindo ao seu novo
+                destino de moda online!
+              </p>
 
-            <button
-              className="mt-4 bg-primary px-2 py-1 rounded-[0.62em] font-bold hover:opacity-80 transition-all"
-              onClick={handleNavigateExploreClick}
-            >
-              Explorar
-            </button>
-          </div>
+              <button
+                className="mt-4 bg-primary px-2 py-1 rounded-[0.62em] font-bold hover:opacity-80 transition-all"
+                onClick={handleNavigateExploreClick}
+              >
+                Explorar
+              </button>
+            </div>
 
-          <div className="max-w-[18.75em] w-full mx-auto md:mx-0">
-            <img
-              src="groupImages.png"
-              alt="Group of Images"
-              className="w-full"
-            />
-          </div>
-        </section>
+            <div className="max-w-[18.75em] w-full mx-auto md:mx-0">
+              <img
+                src="groupImages.png"
+                alt="Group of Images"
+                className="w-full"
+              />
+            </div>
+          </section>
+        </div>
       </main>
       <div className="mt-14">
         <Footer />
